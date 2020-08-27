114b325bf41172fbf754d607ec97a53c78e6d205
Chance Smith <chancesmith97@hotmail.com>
2020-08-27 19:57:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :posts, only: %i[show index new edit]
   end
 
-  get '/stats', to: 'stats#index'
+  
 
   resources :posts, only: %i[index show new create edit update]
 
